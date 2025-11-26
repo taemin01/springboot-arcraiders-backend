77d803a80034be7d664ebffcf5ffdf395e6a00e3
taemin <jirr1010@gmail.com>
2025-11-26 12:06:18 +0900

    first commit

---
+++ src/main/java/org/arc/raders/ArcRadersApplication.java
@@ -0,0 +1,13 @@
+package org.arc.raders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcRadersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcRadersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/arc/raders/ArcRadersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arc.raders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcRadersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
